bc1f0efa67501e41c5e6bf85a676fbf8a039a8fa
stans184 <life4603@naver.com>
2024-01-30 21:23:27 +0900

    logger control

--- logger_config.py
+++ logger_config.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import datetime
 
 def setup_logger():
     logger = logging.getLogger('SAT_logger')
@@ -14,5 +15,23 @@ def setup_logger():
 
     # 로거에 핸들러 추가
     logger.addHandler(file_handler)
+    
+    # 로그 파일 유지 관리
+    manage_log_file('SAT_log.log', 15)
 
     return logger
+
+def manage_log_file(log_file_path, retention_days):
+    retention_date = datetime.datetime.now() - datetime.timedelta(days=retention_days)
+    updated_log_lines = []
+
+    with open(log_file_path, 'r') as file:
+        for line in file:
+            date_str = line.split()[0]
+            log_date = datetime.datetime.strptime(date_str, '%m/%d/%Y')
+
+            if log_date > retention_date:
+                updated_log_lines.append(line)
+
+    with open(log_file_path, 'w') as file:
+        file.writelines(updated_log_lines)
